e5e11363b830bd0499978a9b875fe22cb1235dbd
jhony.hacku <thecrak2006@gmail.com>
2026-09-01 10:43:25 -0500

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/desarrollo/web/Bizagi2/Bizagi2Application.java
@@ -0,0 +1,13 @@
+package desarrollo.web.Bizagi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bizagi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bizagi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/desarrollo/web/Bizagi2/Bizagi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package desarrollo.web.Bizagi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bizagi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
