b6479cb4da1a4b8c3a2c26667f91adf4df095722
xamuel perez <xamuel_perez@javeriana.edu.co>
2026-09-01 20:24:10 -0500

    feat: add JPA repositories for process entities

---
+++ src/main/java/desarrollo/web/Bizagi2/repository/CorrelacionRepository.java
@@ -0,0 +1,8 @@
+package desarrollo.web.Bizagi2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import desarrollo.web.Bizagi2.entities.Correlacion;
+
+public interface CorrelacionRepository extends JpaRepository<Correlacion, Integer> {
+}
---
+++ src/main/java/desarrollo/web/Bizagi2/repository/RolProcesoRepository.java
@@ -0,0 +1,8 @@
+package desarrollo.web.Bizagi2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import desarrollo.web.Bizagi2.entities.RolProceso;
+
+public interface RolProcesoRepository extends JpaRepository<RolProceso, Integer> {
+}
